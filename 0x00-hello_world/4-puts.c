cb0c0411bf6315b2fc1a45c964c335aadad16988
ami_birhanu <lightened1@github.com>
2022-09-10 10:55:39 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #!/bin/bash
 #include <stdio.h>
 
-
-
 /**
  *
- *  * main - Entry point
+ *  *main - entry point
  *
  *   *
  *
- *    * Return: Always 0 (Success)
+ *    *Return: 0 after printing the function
  *
  *     */
 
@@ -17,8 +15,8 @@ int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+	return (0);
 
 }
