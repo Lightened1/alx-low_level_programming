d109841967ac96f8fe3c1b135874345e06767acd
ami_birhanu <lightened1@github.com>
2022-09-13 05:54:38 -0700

    trial

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,39 +1,25 @@
-#include "holberton.h"
+#include <unistd.h>
+
+
 
 /**
  *
- *  * main - Entry point
+ *  * _putchar - writes the character c to stdout
  *
- *   * Description: prints 'Holberton \n'
+ *   * @c: The character to print
  *
- *    * Return: Always 0 (success)
+ *    *
  *
- *     */
+ *     * Return: On success 1.
+ *
+ *      * On error, -1 is returned, and errno is set appropriately.
+ *
+ *       */
 
-int main(void)
+int _putchar(char c)
 
 {
 
-	char str[] = "Holberton\n";
-
-	int i = 0;
-
-
-
-	while (str[i] != '\0')
-
-			{
-
-					char c = str[i];
-
-
-
-						_putchar(c);
-
-							i++;
-
-								}
-
-	return (0);
+		return (write(1, &c, 1));
 
 }
