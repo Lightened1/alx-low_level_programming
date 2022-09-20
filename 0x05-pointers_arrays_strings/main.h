0dcda821078e1da1062ebfee39117398304cf459
ami_birhanu <lightened1@github.com>
2022-09-20 04:53:46 -0700

    df

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,8 +2,8 @@
 #define MAIN_H
 
 /**
-*void prototypes()
-*int prototypes()
+* void prototypes()
+* int prototypes()
 */
 
 void reset_to_98(int *n);
@@ -18,4 +18,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *str);
 
-#endif /*MAIN_H*/
+#endif /*MAIN.H*/
