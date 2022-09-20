9f9530a91920a73c2104ea0a2e4581615edb8cfc
ami_birhanu <lightened1@github.com>
2022-09-20 04:27:09 -0700

    df

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,17 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-* main - check the code 
-*
-* Return: Always 0.
+*reset_to_98 - resets the value of given integer to 98
+*@n: parameter
+*Return : nothing
 */
-int main(void)
+
+void reset_to_98(int *n)
 {
-int n;
-n = 402;
-printf("n=%d\n", n);
-reset_to_98(&n);
-printf("n=%d\n", n);
-return (0);
+	*n = 98;
 }
