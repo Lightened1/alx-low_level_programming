deea8469e55a66eb9ac7e971b0b563df825850bd
ami_birhanu <lightened1@github.com>
2022-09-22 00:55:32 -0700

    fd

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,8 +1,9 @@
-include "main.h"
+#include "main.h"
 
 /**
 *_strncat - concatenate two strings but add inputted number of bytes
-*@dest: string to be appended upon *@src: string to be completed at end of dest
+*@dest: string to be appended upon
+*@src: string to be completed at end of dest
 *@n:integer parameter to compare index to
 *Return: returns new concatenated string
 */
@@ -11,11 +12,11 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int index = 0, dest_len = 0;
 
-	while (dest[index++])
+	while (dest[index++]
 		dest_len++;
 
-for (index = 0; src[index] && index < n; index++)
+	for (index = 0; src[index] && index < n; index++)
 	dest[dest_len++] = src[index];
 
-return (dest);
+	return (dest);
 }
