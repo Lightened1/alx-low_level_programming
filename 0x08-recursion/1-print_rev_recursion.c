a8f239dbdfb5ca4f6cb37a5c8de2ab11149b444f
ami_birhanu <lightened1@github.com>
2022-09-29 02:51:59 -0700

    gf

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,18 @@
 #include "main.h"
 
 /**
-* main - check the code
+* _print_rev_recursion - prints a string in reverse
+* @s: string to be printed
 *
-* Return: Always 0.
+* Return: void
 */
 
-int main(void)
+void _print_rev_recursion(char *s)
 {
-	_print_rev_recursion("\nColton Walker");
-	return (0);
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+
 }
