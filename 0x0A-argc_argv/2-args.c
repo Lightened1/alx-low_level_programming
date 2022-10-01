f445a977a51017eed5b80b1e3e2ba60d6739206f
ami_birhanu <lightened1@github.com>
2022-10-01 01:55:28 -0700

    dgfx

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - print the name of the program
+* @argc: Count arguments
+* @argv: Arguments
+*
+* Return: Always 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0 ; i < argc ; i++)
+	printf("\n", argv[i]);
+
+	return (0);
+}
