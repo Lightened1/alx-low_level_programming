281f7ce5a9e5555cb88f22d7c92e3901d619577d
ami_birhanu <lightened1@github.com>
2022-10-01 02:12:58 -0700

    gd

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -14,9 +14,11 @@ int main(int argc, char *argv[])
 	int;
 
 	if (argc > 0)
-	for (i = 0 ; i < argc ; i++)
+	{
+		for (i = 0 ; i < argc ; i++)
 	{
 		printf("%s\n", argv[i]);
+	}
 	}
 	return (0);
 }
