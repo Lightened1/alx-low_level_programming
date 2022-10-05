246a843a6d6716d40d17be4b30eb7fdc08a39b78
ami_birhanu <lightened1@github.com>
2022-10-05 11:27:06 -0700

    rdgf

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,13 +3,13 @@
 
 #include <stdlib.h>
 
-int putchar(char);
+int _putchar(char);
 char *create_array(unsigned int size, char c);
-char *_str_concat(char *s1, char *s2);
-char *strdup(char *str);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
-char *argstostr(intac, char **av);
+char *argstostr(int ac, char **av);
 char **strtow(char *str);
 
 #endif /* MAIN_H */
