134e19c71de42feff78d0ce7262127d94c1356b3
ami_birhanu <lightened1@github.com>
2022-10-11 13:10:29 -0700

    init dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,4 @@
-/**
+/*
 * File: 1-init_dog.c
 * Auth: AMI BIRHANU
 */
@@ -13,7 +13,6 @@
 * @age: The age of the dog.
 * @owner: The owner of the dog.
 */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
