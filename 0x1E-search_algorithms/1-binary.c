56d5ca117425087859999a81186aee2843c74a03
AMI <lightened1@github.com>
2023-04-12 06:22:58 -0700

    correction

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,61 +1,85 @@
 #include "search_algos.h"
-
+int binary_recur(int *array, size_t start, size_t mid, size_t end, int value);
+void print_array(int *array, size_t start, size_t end);
 /**
- * recursive_search - searches for a value in an array of
- * integers using the Binary search algorithm
- *
- *
- * @array: input array
+ * binary_search - searches an array for a target with the binary
+ * search algorithm
+ * @array: pointer to first element in the array
  * @size: size of the array
- * @value: value to search in
- * Return: index of the number
- */
-int recursive_search(int *array, size_t size, int value)
-{
-	size_t half = size / 2;
-	size_t i;
-
-	if (array == NULL || size == 0)
-		return (-1);
-
-	printf("Searching in array");
-
-	for (i = 0; i < size; i++)
-		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
-
-	printf("\n");
-
-	if (half && size % 2 == 0)
-		half--;
-
-	if (value == array[half])
-		return ((int)half);
-
-	if (value < array[half])
-		return (recursive_search(array, half, value));
-
-	half++;
-
-	return (recursive_search(array + half, size - half, value) + half);
-}
-
-/**
- * binary_search - calls to binary_search to return
- * the index of the number
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
+ * @value: target value to search for
+ * Return: index of the target if it's found, otherwise -1
  */
 int binary_search(int *array, size_t size, int value)
 {
-	int index;
+	size_t start, end, mid;
 
-	index = recursive_search(array, size, value);
-
-	if (index >= 0 && array[index] != value)
+	if (!array)
 		return (-1);
 
-	return (index);
+	start = 0;
+	end = size - 1;
+	mid = (start + end) / 2;
+	print_array(array, 0, size - 1);
+
+	return (binary_recur(array, start, mid, end, value));
+}
+
+/**
+ * binary_recur - recursive function for the binary search algorithm
+ * @array: pointer to the array
+ * @start: starting position of search
+ * @end: terminating position of search
+ * @mid: midpoint of array
+ * @value: value to be searched for
+ * Return: position of value, otherwise -1
+ */
+int binary_recur(int *array, size_t start, size_t mid, size_t end, int value)
+{
+	size_t pos;
+
+	if (start >= end || end <= start)
+		return (-1);
+
+	if (array[mid] == value)
+		return (mid);
+
+	if (array[mid] < value)
+	{
+		start = mid + 1;
+		mid = (start + end) / 2;
+		print_array(array, start, end);
+		pos = binary_recur(array, start, mid, end, value);
+	}
+	if (array[mid] > value)
+	{
+		end = mid - 1;
+		mid = (start + end) / 2;
+		print_array(array, start, end);
+		pos = binary_recur(array, start, mid, end, value);
+	}
+	return (pos);
+}
+
+/**
+ * print_array - prints the array/subarray currently being searched
+ * @array: pointer to the array
+ * @start: starting position to print
+ * @end: terminting position
+ * Return: nothing
+ */
+void print_array(int *array, size_t start, size_t end)
+{
+	size_t i;
+	char *sep = "";
+
+
+	printf("Searching in array: ");
+	i = start;
+	while (i <= end)
+	{
+		printf("%s%d", sep, array[i]);
+		sep = ", ";
+		i++;
+	}
+	printf("\n");
 }
